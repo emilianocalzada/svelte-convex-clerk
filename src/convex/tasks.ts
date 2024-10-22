31537f795f6bf1dcea62a89c5e266a294bf4ba99
emilianocalzada <dcm74276@lasallebajio.edu>
2024-10-22 17:05:08 +0200

    add tasks table and functionality for user auth test

--- src/convex/tasks.ts
+++ src/convex/tasks.ts
@@ -1,12 +1,38 @@
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
 
 export const get = query({
     args: {},
     handler: async (ctx) => {
-        const user = await ctx.auth.getUserIdentity();
-        console.log("user", user);
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+            throw new ConvexError("Not authenticated");
+        }
 
-        const tasks = await ctx.db.query("tasks").collect();
-        return tasks.map((task) => ({ ...task, assigner: "tom" }));
+        console.log(JSON.stringify(identity));
+
+        const tasks = await ctx.db.query("tasks")
+            .withIndex("by_userId", (q) => q.eq("userId", identity.subject))
+            .collect();
+        return tasks
+    },
+});
+
+export const create = mutation({
+    args: {
+        text: v.string(),
+        isCompleted: v.boolean(),
+    },
+    handler: async (ctx, { text, isCompleted }) => {
+        const identity = await ctx.auth.getUserIdentity();
+        if (identity === null) {
+            throw new ConvexError("Not authenticated");
+        }
+
+        const task = await ctx.db.insert("tasks", {
+            text,
+            isCompleted,
+            userId: identity.subject,
+        });
     },
 });
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import { useQuery } from 'convex-svelte';
 	import { api } from '../../convex/_generated/api';
+	import { useConvexClient } from 'convex-svelte';
 
 	const query = useQuery(api.tasks.get, {});
+
+	const convex = useConvexClient();
+	async function addTask(event: Event) {
+		event.preventDefault();
+		const form = event.target as HTMLFormElement;
+		const formData = new FormData(form);
+		const text = formData.get('text') as string;
+		const isCompleted = formData.get('isCompleted') === 'on';
+		await convex.mutation(api.tasks.create, { text, isCompleted });
+	}
 </script>
 
 {#if query.isLoading}
@@ -10,12 +21,16 @@
 {:else if query.error}
 	failed to load: {query.error.toString()}
 {:else}
+	<form on:submit={addTask}>
+		<input type="text" name="text" placeholder="Task text" />
+		<input type="checkbox" name="isCompleted" />
+		<button type="submit">Add</button>
+	</form>
 	<ul>
 		{#each query.data as task}
 			<li>
 				{task.isCompleted ? '☑' : '☐'}
 				<span>{task.text}</span>
-				<span>assigned by {task.assigner}</span>
 			</li>
 		{/each}
 	</ul>
